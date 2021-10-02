1e75a29fa72979ee276e76df25ba3c0c97c5e4b5
João Paulo <joaopaulomr96@gmail.com>
2021-10-02 14:34:09 -0300

    v1.1

--- q4/script.js
+++ q4/script.js
@@ -18,56 +18,87 @@ btn1.addEventListener("click", function(e) {
     
     let valor = 0
 
-    if (suco.checked==true || refrigerante.checked==true || agua.checked==true) {
-        document.querySelector('#listaBebidas').innerHTML = "Bebidas consumidas:"
-    }
+    if(nome != ''){
+        /////////////
+        //Bebidas////
+        /////////////
+    
+        if (suco.checked==true || refrigerante.checked==true || agua.checked==true) {
+            listaBebidas.innerHTML = "Bebidas consumidas: "
+            if (suco.checked==true) {
+                valor += 5
+                liBebidas = document.createElement("li")
+                liBebidas.textContent = "Suco"
+                listaBebidas.appendChild(liBebidas)
+            }
+            if (refrigerante.checked==true) {
+                valor += 4,5
+                liBebidas = document.createElement("li")
+                liBebidas.textContent += "Refrigerante"
+                listaBebidas.appendChild(liBebidas)
+            }
+            if (agua.checked==true) {
+                valor += 1,5
+                liBebidas = document.createElement("li")
+                liBebidas.textContent = "Água"
+                listaBebidas.appendChild(liBebidas)
+            }
+        }
 
-    if (suco.checked==true) {
-        valor += 5
-        liBebidas.textContent = "Suco"
-        listaBebidas.appendChild(liBebidas)
-    }
-    if (refrigerante.checked==true) {
-        valor += 4,5
-        liBebidas.textContent = "Refrigerante"
-        listaBebidas.appendChild(liBebidas)
-    }
-    if (agua.checked==true) {
-        valor += 1,5
-        liBebidas.textContent = "Água"
-        listaBebidas.appendChild(liBebidas)
-    }
+        //////////////
+        //Comidas////
+        /////////////
 
-    if (bolo.checked==true || pastel.checked==true || torta.checked==true) {
-        document.querySelector('#tituloComidas').innerHTML = "Doces e salgados consumidos:"
+        if (bolo.checked==true || pastel.checked==true || torta.checked==true) {
+            listaComidas.innerHTML = "Doces e salgados consumidos: "
+            if (bolo.checked==true) {
+                valor += 4,5
+                liComidas = document.createElement("li")
+                liComidas.textContent = "Bolo"
+                listaComidas.appendChild(liComidas)
+            }
+            if (pastel.checked==true) {
+                valor += 5
+                liComidas = document.createElement("li")
+                liComidas.textContent = "Pastel"
+                listaComidas.appendChild(liComidas)
+            }
+            if (torta.checked==true) {
+                valor += 6
+                liComidas = document.createElement("li")
+                liComidas.textContent = "Torta"
+                listaComidas.appendChild(liComidas)
+            }
+        }
     }
+    ///////////////
+    ///Resultado///
+    ///////////////
 
-    if (bolo.checked==true) {
-        valor += 4,5
-        liComidas.textContent = "Bolo"
-        listaComidas.appendChild(liComidas)
-    }
-    if (pastel.checked==true) {
-        valor += 5
-        liComidas.textContent = "Pastel"
-        listaComidas.appendChild(liComidas)
-    }
-    if (torta.checked==true) {
-        valor += 6
-        liComidas.textContent = "Torta"
-        listaComidas.appendChild(liComidas)
-    }
-
-    if (valor == 0){
-        window.alert("Nenhum produto consumido")
-    } else {
+    if (nome != '' && valor != 0) {
+        document.querySelector('#comanda').innerHTML = "- Comanda -"
         document.querySelector('#infoCliente').innerHTML = "Cliente: " + nome
         document.querySelector('#infoValor').innerHTML = "Valor: " + valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
     }
+
+    if (nome == ''){
+        window.alert("Nenhum nome informado")
+    }
+    if (suco.checked==false && refrigerante.checked==false && agua.checked==false && bolo.checked==false && pastel.checked==false && torta.checked==false){
+        window.alert("Nenhum produto consumido")
+    }
 })
+
+    //////////////
+    ///Limpar/////
+    //////////////
 
 const btn2 = document.querySelector('#clean')
 
 btn2.addEventListener("click", function(e) {
-
+    document.querySelector('#comanda').innerHTML = null
+    document.querySelector('#infoCliente').innerHTML = null
+    document.querySelector('#listaBebidas').innerHTML = null
+    document.querySelector('#listaComidas').innerHTML = null
+    document.querySelector('#infoValor').innerHTML = null
 })
